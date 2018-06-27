823b6a1ec86f807fad82d38c7c12c155be1989d5
Cuervachoos <38992505+Cuervachoos@users.noreply.github.com>
2018-06-27 20:56:06 -0300

    agenda telefonica del ejercicio de la agenda

--- AgendaCompartida/src/agendacompartida/dao/TelefonoDao.java
+++ AgendaCompartida/src/agendacompartida/dao/TelefonoDao.java
@@ -1,10 +1,26 @@
-
 package agendacompartida.dao;
+
+import agendacompartida.dominio.Telefono;
 
 /**
  *
  * @author AsusX541U
  */
 public class TelefonoDao {
-    
+
+    private final Conexion conexion;
+
+    public TelefonoDao(Conexion c) {
+        this.conexion = c;
+    }
+
+    public void updateTelefono(Telefono telefono) {
+        conexion.ejecutarSQL(
+                "update Telefonos "
+                + "set numero = " + telefono.getNumero()
+                + " , codArea = " + telefono.getPrefijo()
+                +", tipo = "+ telefono.getTipoTelefono());
+
+    }
 }
+
