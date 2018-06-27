271022fd302034562165ff25766cf805fa4335c6
alumno <alumno@913-m13.gabinete.frc.utn.edu.ar>
2018-06-27 20:48:44 -0300

    eliminar

--- AgendaCompartida/src/agendacompartida/dao/ContactoDao.java
+++ AgendaCompartida/src/agendacompartida/dao/ContactoDao.java
@@ -3,14 +3,14 @@ package agendacompartida.dao;
 
 public class ContactoDao
 {
-public void eliminarContacto (String id){
-	
-		
-		}    
+
     
     public void insertarDatos(int dni, String nombre, String apellido, String direccion, Date fechaNac, Telefono[] tel){
         
     }
     
-    
+    public void eliminarContacto (String id){
+	
+		
+		}    
 }
