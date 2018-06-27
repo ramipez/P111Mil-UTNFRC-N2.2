8005db410a33b8ac598e210e0909fb2396177277
mrj11 <>
2018-06-27 20:40:12 -0300

    eliminar

--- AgendaCompartida/src/agendacompartida/dao/ContactoDao.java
+++ AgendaCompartida/src/agendacompartida/dao/ContactoDao.java
@@ -3,6 +3,10 @@ package agendacompartida.dao;
 
 public class ContactoDao
 {
+public void contacto(String nombre){
+	
+		
+		}    
     
     public void insertarDatos(int dni, String nombre, String apellido, String direccion, Date fechaNac, Telefono[] tel){
         
